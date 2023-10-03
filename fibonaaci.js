2ff80e1dbfaf31847df8f15e2fa62daa296be79e
KATE_FISCHER <KATME2006@GMAIL.COM>
2023-10-03 07:29:28 -0500

    First Commit Fibonacci Sequence

---
+++ fibonaaci.js
@@ -0,0 +1,21 @@
+function fibonacci(num) {
+  //Array to begin Fibonacci Sequence
+  fibArr = [0, 1];
+
+  if (num === 1) {
+    return fibArr[0];
+  } else {
+    //Loop to adding together two numbers and replacing last element in array
+    for (i = 2; i < num; i++) {
+      fibArr.push(fibArr[0] + fibArr[1]);
+      fibArr.shift();
+    }
+  }
+
+  const fibNum = fibArr[0] + fibArr[1];
+
+  return fibNum;
+}
+
+console.log(fibonacci(4)); //3
+console.log(fibonacci(12)); //144
