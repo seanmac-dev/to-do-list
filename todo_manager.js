75fa56303cc5ef2795c06924e2a0d84630f352ba
chadeqmartin <chadeqmartin@gmail.com>
2023-10-06 17:53:46 -0400

    added prompt functionality

--- todo_manager.js
+++ todo_manager.js
@@ -22,6 +22,40 @@ const taskRemove = (userInput) => {
 const viewList = () => {
     return Object.keys(task)
 }
-console.log(taskAdd("first task"))
+
+
+
+//prompt for userInput
+const toDoListShell = () => {
+    //let selection = prompt("Enter 1: Add a task \n Enter 2: Modify \n Enter 3: Delete\n Enter 4: View List")
+    const readline = require("readline");
+
+    // create interface for input and output
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    
+    // create empty user input
+    let userInput = "";
+    
+    // question user to enter name
+    rl.question("What is your name\n", function (string) {
+      userInput = string;
+    
+      console.log("Your name is " + userInput);
+    
+      // close input stream
+      rl.close();
+
+    });
+    // if (selection === '1'){
+    //    let taskName = prompt("Enter a task");
+    //    taskAdd(taskName)
+    // }
+    
+}
+console.log(toDoListShell())
+//console.log(taskAdd("first task"))
 
 module.exports = {taskAdd: taskAdd, taskComplete: taskComplete, taskRemove: taskRemove, viewList: viewList}
