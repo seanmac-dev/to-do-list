c0435689630808468597f938a5a1d544d1095ff2
KATE_FISCHER <KATME2006@GMAIL.COM>
2023-10-06 17:36:17 -0500

    Final version

--- To-Do-Lis/to_do.py
+++ To-Do-Lis/to_do.py
@@ -6,26 +6,26 @@ def to_do():
     n = True
 
     while n == True:
-        menu_prompt ='To do list options: \n 1.Add an item\n 2.Mark Task as Complete \n 3.Delete Task \n 4.View Task \n 5.Exit \nPlease enter an option: '
-
+        menu_prompt ='\nTo do list options: \n 1.Add an item\n 2.Mark Task as Complete \n 3.Delete Task \n 4.View Task \n 5.Exit \nPlease enter an option: '
+        if n == False:
+            pass
         user_input = input(menu_prompt)
 
         if user_input == '1':
-            to_do_name = input('Please enter task name: ')
+            to_do_name = input('\nPlease enter task name: ')
             add_item(to_do_name)     
         elif user_input == '2':
-            to_cross_off = input('Please enter task to cross off: ')
+            to_cross_off = input('\nPlease enter task to cross off: ')
             mark_as_done(to_cross_off)
         elif user_input == '3':
-            to_delete = input('What task do you want to delete?')
+            to_delete = input('\nWhat task do you want to delete?')
             delete_Task(to_delete)
         elif user_input == '4':
             view_list()
         elif user_input == '5':
-          
             n = False
         else:
-            print('Invalid Input')
+            print('\nInvalid Input')
 
 
 
@@ -35,7 +35,7 @@ def add_item(to_do_name):
    
     tasks[to_do_name] = False
     print('Task Added')
-    to_do()
+    
 
 def mark_as_done(to_do_name):
     #check if the item is on list
@@ -44,14 +44,14 @@ def mark_as_done(to_do_name):
         cross_off = f'X {to_do_name}'
         tasks[cross_off] = tasks[to_do_name]
         del tasks[to_do_name]
-        to_do()
+        
 
 def delete_Task(to_do_name):
     del tasks[to_do_name]
 
 def view_list():
     print(tasks)
-    to_do()
+    
   
 
 
